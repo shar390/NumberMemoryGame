e524d66716af97d234211c34f6af88f7c670717c
shar390 <144183206+shar390@users.noreply.github.com>
2023-10-14 18:03:04 +0300

    Code the Functions and main Function

--- src/NumberMemory.java
+++ src/NumberMemory.java
@@ -1,10 +1,80 @@
+import java.util.Random;
+import java.util.Scanner;
 
-public class NumberMemory {
+public class NumberMemory{
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello");
+        
+        int[] randomNumbers = new int[7];
+        for (int i = 0; i < 7; i++) {
+            randomNumbers[i] = random.nextInt(5) + 1;
+        }
 
-	}
+        System.out.println("Try to remember the following numbers. The numbers will be shown for 4 seconds.");
+        displayNumbers(randomNumbers, 4000); 
 
+        
+        clearScreen();
+
+       
+        int[] userNumbers = new int[7];
+        for (int i = 0; i < 7; i++) {
+            System.out.print("Type number " + (i + 1) + ": ");
+            userNumbers[i] = scanner.nextInt();
+        }
+
+        
+        System.out.print("The numbers were: ");
+        displayNumbers(randomNumbers, 0); 
+
+        
+        System.out.print("Your numbers were: ");
+        displayNumbers(userNumbers, 0); 
+
+        
+        int correctCount = countCorrectAnswers(randomNumbers, userNumbers);
+        double percentage = ((double) correctCount / 7) * 100;
+        System.out.println("Number of correct answers: " + correctCount);
+        System.out.printf("Percentage of correct answers: %.2f%%\n", percentage);
+
+        scanner.close();
+    }
+
+    
+    private static void displayNumbers(int[] numbers, long duration) {
+        long startTime = System.currentTimeMillis();
+        long endTime = startTime + duration;
+
+        while (System.currentTimeMillis() < endTime) {
+            for (int number : numbers) {
+                System.out.print(number);
+            }
+            System.out.println();
+            try {
+                Thread.sleep(1000); 
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    
+    private static int countCorrectAnswers(int[] correctNumbers, int[] userNumbers) {
+        int count = 0;
+        for (int i = 0; i < correctNumbers.length; i++) {
+            if (correctNumbers[i] == userNumbers[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    
+    private static void clearScreen() {
+        for (int i = 0; i < 20; i++) {
+            System.out.println();
+        }
+    }
 }
